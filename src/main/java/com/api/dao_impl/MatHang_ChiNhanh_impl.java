a700389c0470fd7f3bc94fd7c57f5eb641557a9d
DELL-DOANHIEU <doanhieudtu@gmail.com>
2018-09-14 16:54:28 +0700

    Rejected commit: Default

---
+++ src/main/java/com/api/dao_impl/MatHang_ChiNhanh_impl.java
@@ -0,0 +1,12 @@
+package com.api.dao_impl;
+
+import com.api.dao_interface.MatHang_ChiNhanh_Dao;
+import com.api.model.MatHang_ChiNhanh;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by DELL on 09/14/2018.
+ */
+@Repository
+public class MatHang_ChiNhanh_impl extends  AbstractImpl<MatHang_ChiNhanh, Integer> implements MatHang_ChiNhanh_Dao{
+}
