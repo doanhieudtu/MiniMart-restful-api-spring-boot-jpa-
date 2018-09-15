748f36ae113201dfe5b24832598629066742a2a3
DELL-DOANHIEU <doanhieudtu@gmail.com>
2018-09-16 00:48:56 +0700

    Rejected commit: Default

---
+++ src/main/java/com/api/dao_interface/MatHang_ChiNhanh_Dao.java
@@ -0,0 +1,9 @@
+package com.api.dao_interface;
+
+import com.api.model.MatHang_ChiNhanh;
+
+/**
+ * Created by DELL on 09/14/2018.
+ */
+public interface MatHang_ChiNhanh_Dao extends GenericDao<MatHang_ChiNhanh,Integer> {
+}
